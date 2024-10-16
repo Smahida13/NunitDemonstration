85d6ce8a29d52c6f47abe8cc1263dd659e361f27
smahi <smahi@MSI>
2024-10-16 15:41:35 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+    namespace NUnitDemonstration
+    {
+    //Surushti Mahida
+        public static class Triangle
+        {
+            public static string ValidTriangle(int firstAngle, int secondAngle, int
+            thirdAngle)
+            {
+                string result;
+                if ((firstAngle + secondAngle - thirdAngle) == 90)
+                {
+                    result = "The triangle is rightangle.";
+                }
+                else
+                {
+                    result = "The triangle is NOT rightangle.";
+                }
+                return result;
+            }
+        }
+    }
+
+
